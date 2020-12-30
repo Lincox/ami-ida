49ff71f49f34e483b68af38f29ab976374cf8747
Le Ngoc Minh <l.n.minh@alive-web.co.jp>
2020-12-30 13:42:27 +0700

    amiida life

--- dist/wp/wp-content/themes/wp-templ/archive-amiida_life.php
+++ dist/wp/wp-content/themes/wp-templ/archive-amiida_life.php
@@ -20,32 +20,52 @@ include(APP_PATH.'libs/head.php');
   </div>
   <div class="amiidalife wcm">
     <div class="amiidalife-lst">
-      <?php $ttl= array("<em>仕事に子育て</em>など、、<br>追われるように過ぎていく<br>あっという間の毎日","ふと、イライラしている自分に気付く。<br>ワタシ、<em>心にゆとり</em>がなくなって<br>しまっている、、？","最近通い始めたアミーダは、<br>溶岩石で<em>身体を温める</em>ヨガ。<br>忙しい日々の中でも、<br>なぜか<em>通い続ける</em>ことができている","アミーダの溶岩ヨガで習った、<br><em>“呼吸”</em>が、心のバランスを<br>整えることを知った。<br>ストレスを感じづらくなったのは<br>呼吸のおかげかもしれない。","アミーダの溶岩石には、<br>保温と発汗作用の効果がある。<br>「<em>深い呼吸</em>」が身体だけでなく、<br>毎日の心まで健康にしてくれた。");
-      for($i=0;$i<count($ttl);$i++){ ?>
-      <div class="item inview fadeInBottom">
-        <div class="item__img">
-          <div class="item--ttl sp">
-            <p class="ttl1"><?php echo $ttl[$i]; ?></p>
+      <div class="item">
+        <div class="item__intro">
+          <div class="img">
+            <img src="<?php echo APP_ASSETS; ?>img/amiidalife/1.jpg" alt="">
           </div>
-          <div class="img lazy" data-bg="url(<?php echo APP_ASSETS; ?>img/amiidalife/img<?php echo $i+1; ?>.jpg)"></div>
-        </div>
-        <div class="item__info">
-          <div class="item--ttl pc">
-            <p class="ttl1"><?php echo $ttl[$i]; ?></p>
-          </div>
-          <div class="item__info--space"></div>
-          <div class="item__info--img">
-            <div class="img lazy" data-bg="url(<?php echo APP_ASSETS; ?>img/amiidalife/img<?php echo ($i+1).'_'.($i+1); ?>.jpg)"></div>
+          <div class="info">
+            <p class="ttl">アミーダのある生活で、<br>仕事も家庭も、毎日が快適。</p>
+            <p class="txt">Yさん（40代 / 会社員）<br>アミーダ歴：4年</p>
           </div>
         </div>
-        <div class="line">
-          <div class="space"> </div>
-          <div class="inner">
-            <div class="circle1"></div><div class="circle2"></div>
+        <div class="item__detail">
+          <p class="ttl">アミーダに<br>入会された<em>きっかけ</em>は？</p>
+          <div class="img">
+            <img src="<?php echo APP_ASSETS; ?>img/amiidalife/3.jpg" alt="">
           </div>
+          <p class="txt">仕事をしながら、体力が落ちているなって感じることがありました。体調不良や持病などはないのですが、会社がすごく遠くて、通勤だけでも結構体力使うので、体力を付けたいなと思っていたんです。<br><br>そんな時ちょうど近くにアミーダがオープンし、「ホットヨガって、どんな感じなのかな？」と思って体験に行ったのがきっかけでした。それからもう4年通い続けていますね。未経験でしたが、とてもアットホームな雰囲気で、躊躇なく入会できたのが印象に残っています。<br><br>新型コロナウィルスの影響でアミーダも休業になった時、体力がどんどん落ちていくのを感じていて、こんなにも私の日常を支えられていたのかと、少しでも早い再開を願っていました。今は安心安全の対策もしっかりされていて、再開してもらってありがたいです。</p>
+        </div>
+        <div class="item__detail">
+          <p class="ttl">アミーダのある生活を<br>4年続けて感じる、<br>体や心の<em>変化</em>やエピソードを<br>教えてください。</p>
+          <div class="img">
+            <img src="<?php echo APP_ASSETS; ?>img/amiidalife/3.jpg" alt="">
+          </div>
+          <p class="txt">通い始めてから半年くらい経った頃、「あれ？少しずつ筋肉がついてきたかな？」と体が変わっていくのがとても実感できるようになりました。代謝もよくなって、レッスンの最初から汗も出るようあになっていました。<br><br>最初の1年間で、体重が8キロも減って体脂肪は9%くらい落ちたと思います。体重の増減もなくなりましたね。体重だけではなく、基礎体温も1℃ほど高くなり、免疫力が強くなったようにも感じます。<br>こんなに体の変化があるので、こちらから話をしなくても、「何かやってるの？」と周りのみんなに聞かれることは多いですね。<br><br>あとは、「ヨガの話をしている時ってすごい楽しそうだよね。」って言われます。多分本当に楽しいので、それがバレてるのかもしれません。話しているうちに「私もやろうかな」と言って、ヨガに通う友達も増えてきました。<br><br>毎日快適に過ごせる事が本当にありがたくて、もうやめられないですね。</p>
+        </div>
+        <div class="item__detail">
+          <p class="ttl">どのくらいの<em>ペース</em>で<br>通われていますか？<br>どんなレッスンが<em>好き</em>ですか？</p>
+          <div class="img">
+            <img src="<?php echo APP_ASSETS; ?>img/amiidalife/4.jpg" alt="">
+          </div>
+          <p class="txt">土日は2レッスン。平日は仕事があるので、夜の最後のレッスンにしかいけないのですが、なるべく通っています。<br><br>もう、アミーダにいくことを中心にスケジュールを組んでいますね。家族からも「今日はヨガ行かなくていいの？」と聞かれるくらいです（笑）アミーダに行った日の私の機嫌が良いことを家族も知っているのでしょうね。気兼ねなく行ける家族のサポートもありがたいです。<br><br>レッスンは、結構ハードな方が好きです。「ホットフロー」など好きですね。仕事で疲れている時は、ゆっくりめの「ハタヨガ」も好きです。<br>いろんなレベルのレッスンがあるので、その日の調子に合わせて、レッスンを選べるのが嬉しいですし、楽しいですね。</p>
+        </div>
+        <div class="item__detail">
+          <p class="ttl">アミーダの店舗の<br><em>雰囲気</em>はどうですか？</p>
+          <div class="img">
+            <img src="<?php echo APP_ASSETS; ?>img/amiidalife/5.jpg" alt="">
+          </div>
+          <p class="txt">4年も通っているので、スタッフの方が変わることも経験しましたが、新しく来る方来る方、毎回とても素敵な方ばかりで、その度にこちらも新鮮な気持ちにさせられます。インストラクターの方はもちろん健康的ですし、内面も綺麗な方が多いと感じています。<br>とにかく明るくてアットホームな雰囲気なんですよね。</p>
+        </div>
+        <div class="item__detail">
+          <p class="ttl">最後に。<br>今、<em>目標</em>にしていることを<br>教えてください。</p>
+          <div class="img">
+            <img src="<?php echo APP_ASSETS; ?>img/amiidalife/5.jpg" alt="">
+          </div>
+          <p class="txt">目標としては、あと２キロ痩せたいと思っています。そのためにアミーダのヨガがなくてはならないので、本当に感謝しています。<br>緊急事態宣言でスタジオに来れなくなった時はとても辛かったです。自宅でストーブやアロマを焚きながら、YouTubeで配信されるアミーダのレッスン動画を見て、体を動かしましたが、汗の質も違うし、全てが物足りなかったです。<br>とにかく、ずっと続けたいと思っているので、今後ともよろしくお願いします！</p>
         </div>
       </div>
-    <?php } ?>
     </div>
   </div>
 </main>
